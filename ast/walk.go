8e1bf2efbf273d87ee509380384ae34599ee6136
Martin Steffen <msteffen@ifi.uio.no>
2015-07-16 11:52:22 +0200

    bug

--- ast/walk.go
+++ ast/walk.go
@@ -17,12 +17,32 @@ func main () {
 	
 }
 
+
+// Originally I thought perhaps one could have an ``easy'' visitor, at
+// least inside one package. However, the following is not possible:
+// func (s *Stmt) print_stmt () {
+//	fmt.Println("stmt") .....
+// }
+
+
+// The problem is: Stmt is an interface, and that's not allowed as receiver
+// for a method. Thefore, one cannod hand a statement over to that function
+// and then make a big case distinction.  What does work instead is a
+// function:
+
+
+ // func  print_stmt (s *Stmt) {
+ // 	 switch sw := s.(type)
+ // 	 case *
+ // default:  	 fmt.Println("stmt")
+
+ // }
+
+
+
 // It seems that inside the same package, one can ``attach'' methods to
 // types defined in a different file. Across packages that does not work.
 
-//func (Stmt) print_stmt () {
-//	fmt.Println("stmt")
-//}
 
 func ( *FACTOR ) print_factor () {fmt.Println("factor")}
 
