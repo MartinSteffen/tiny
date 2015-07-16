2ba793491c7ede66e54f063ce862ee4a1331e693
Martin Steffen <msteffen@ifi.uio.no>
2015-07-16 10:49:47 +0200

    some tests

--- ast/walk.go
+++ ast/walk.go
@@ -1,3 +1,21 @@
 // This is to test what ``local'' and ``non-local'' means wrt. methods.
 
 package ast
+
+import ("fmt") // can't hurt
+
+
+
+// Unlike in the package "test" which is in a different directory, here we
+// can leave out to import "ast" (obvourly) and also we don't need to write
+// ast.SIMPLEEXPR. etc
+
+func main () {
+	f :=    &SIMPLEEXPR{&TERM{&FACTOR{&NUMBER{1}}}}
+	fmt.Println(f)
+	
+	
+}
+
+
+
