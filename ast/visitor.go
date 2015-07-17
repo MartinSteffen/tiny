a1ea6c9532ad7b4cc8cae32b6e89cf90c3688025
Martin Steffen <msteffen@ifi.uio.no>
2015-07-17 09:54:00 +0200

    intermediate

--- ast/visitor.go
+++ ast/visitor.go
@@ -18,5 +18,12 @@ type Visitor interface {
 // The main function in that file is "Walk", which is the visitor
 // functionality. The visitor is invoked on a node of type "Node" which is
 // the catch-all interface for all nodes in the ast there. Inside the
-// visitor, there's a flat switch over all 
+// visitor, there's a flat switch over all.
+
+// The ``visitor'' pattern is basically a higher-order function. It applies
+// the function argument to all nodes of the structure. In the go
+// implementation, the way it's done is flat. This is specified via the
+// Visitor interface, which requires the Visit-function. In a way, it's not
+// so clear if it's a function or method, because the Visit is used in the
+// code in the form v.Visit(n), where n is a node.
 
