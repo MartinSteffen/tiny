3541d1f005a132494e26627dc22ae8bccd0c2aeb
Martin Steffen <msteffen@ifi.uio.no>
2015-07-30 12:11:50 +0200

    intermediate

---
+++ example/example.go
@@ -0,0 +1,27 @@
+package example
+import ("github.com/MartinSteffen/tiny/absynt"
+)
+
+
+var F = &absynt.ID{I:"s"}   // factor
+var T = &absynt.FACTOR{F}   // term
+var SE = &absynt.TERM{T}    // simple expr
+var E = &absynt.SIMPLEEXPR{SE}  // expr 
+var SR = &absynt.READ{I:"x"}    // read stmt
+var SL1 = SR             // stmt (list) 
+var SL2 = SL1            // stmt (list) 
+var S = &absynt.IF{E,SL1,SL2}     // stmt 
+var AO =  &absynt.PLUS{}           // add op "+"
+var F2 = &absynt.NUMBER{N:42}      // factor
+var T2 = &absynt.FACTOR{F2}      // term
+var SE2 =  &absynt.TERM{T2}       // simple expression
+var AO4 = &absynt.MINUS{}         // add op "-"
+var F5  = &absynt.ID{"f5"}
+var T5 = &absynt.FACTOR{F5}
+var SE5 = &absynt.TERM{T5}      
+var SE4 =  &absynt.ADDEXPR{AO4,SE5,T2}    // simple expression
+var E3  = &absynt.SIMPLEEXPR{SE4} // expression
+var F3  = &absynt.EXPR{E3}      // factor
+var T3 = &absynt.FACTOR{F3}           // term
+var SE3 = &absynt.ADDEXPR{AO4,SE2,T3}  // simple expression
+var S2 = &absynt.REPEAT{S,E3}     // stmt 
--- test/visitordirectcount.go
+++ test/visitordirectcount.go
@@ -1,7 +1,9 @@
 package main
 import ("github.com/MartinSteffen/tiny/absynt"
+	"github.com/MartinSteffen/tiny/example"
 	"fmt"
 )
+
 
 
 type visitor struct {count int}   // we cannot make it as pointer to a struct
@@ -15,45 +17,45 @@ func (v *visitor) VisitStmt(s absynt.Stmt) {
 }
 
 
-func (v visitor) VisitExpr(e absynt.Expr) {
+func (v *visitor) VisitExpr(e absynt.Expr) {
 	fmt.Println("send Expr")
 	v.count = v.count + 1
 }
 
 
-func (v visitor) VisitSimpleExpr(e absynt.SimpleExpr) {
+func (v *visitor) VisitSimpleExpr(e absynt.SimpleExpr) {
 	fmt.Println("send SimpleExpr")
 }
 
 
-func (v visitor) VisitIdent(i absynt.Ident)  {
+func (v *visitor) VisitIdent(i absynt.Ident)  {
 	fmt.Println("I(", i, ")")
 	v.count = v.count + 1	
 }
 
 
-func (v visitor) VisitCompareOp(co absynt.CompareOp) {
+func (v *visitor) VisitCompareOp(co absynt.CompareOp) {
 	fmt.Println("CompareOp(...)")
 	v.count = v.count + 1
 }
 
-func (v visitor) VisitTerm(t absynt.Term)  {
+func (v *visitor) VisitTerm(t absynt.Term)  {
 	fmt.Println("Term(...)")
 	v.count = v.count + 1	
 }
 
-func (v visitor) VisitFactor(t absynt.Factor)  {
+func (v *visitor) VisitFactor(t absynt.Factor)  {
 	fmt.Println("Factor(...)")
 	v.count = v.count + 1	
 }
 
-func (v visitor) VisitAddOp(ao absynt.AddOp)  {
+func (v *visitor) VisitAddOp(ao absynt.AddOp)  {
 	fmt.Println("AddOp(...)")
 	v.count = v.count + 1	
 
 }
 
-func (v visitor) VisitMulOp(s absynt.MulOp) {
+func (v *visitor) VisitMulOp(s absynt.MulOp) {
 	fmt.Println("MulOp")
 	v.count = v.count + 1	
 
@@ -61,13 +63,13 @@ func (v visitor) VisitMulOp(s absynt.MulOp) {
 
 
 
-func (v visitor) VisitNumber(n absynt.Number)  {
+func (v *visitor) VisitNumber(n absynt.Number)  {
 	fmt.Println("Number(...)")
 	v.count = v.count + 1	
 
 }
 
-func (v visitor) VisitSymbol(s absynt.Symbol)  {
+func (v *visitor) VisitSymbol(s absynt.Symbol)  {
 	fmt.Println("Symbol(...)")
 	v.count = v.count + 1	
 }
@@ -75,7 +77,7 @@ func (v visitor) VisitSymbol(s absynt.Symbol)  {
 
 //-----------------------------------------------------------------
 // Some concrete tree
-
+var x = example.f 
 var f = &absynt.ID{I:"s"}   // factor
 var t = &absynt.FACTOR{f}   // term
 var se = &absynt.TERM{t}    // simple expr
