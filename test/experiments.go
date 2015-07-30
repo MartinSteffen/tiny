edefaea210b0706103981eb174ea8729ac3beba1
Martin Steffen <msteffen@ifi.uio.no>
2015-07-30 11:33:17 +0200

    OK

--- test/experiments.go
+++ test/experiments.go
@@ -3,12 +3,22 @@ import ("fmt")
 
 
 type record struct{x int}
+
+func inc (s  *record) {
+	s.x = 7
+}
+
 func main () {
 	r:= record{4}
 	fmt.Println("1",r)
 	r.x = 5
 	fmt.Println("2",r)
+	inc(&r)
+	fmt.Println("2",&r)
+	
 }
+
+
 
 
 
