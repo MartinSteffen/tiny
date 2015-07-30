2f5612d7d9fddce7de77e2a6265e523b181212b7
Martin Steffen <msteffen@ifi.uio.no>
2015-07-30 09:47:09 +0200

    remove the prints from the walker

--- absynt/walk.go
+++ absynt/walk.go
@@ -1,5 +1,5 @@
 package absynt  // ast2 might not work
-import ("fmt")
+
 
 type Visitor interface {
 	VisitStmt (Stmt)             (Visitor)
@@ -22,34 +22,23 @@ func WalkStmt (v Visitor, s Stmt) {
 	v.VisitStmt(s)          // action 
 	switch ts := s.(type) { // type assertion
 	case *IF:
-		fmt.Println ("IF (")
 		WalkExpr (v, ts.E)   
 		WalkStmt (v, ts.SL1)
 		WalkStmt (v, ts.SL2)
-		fmt.Println (")")
 	case *READ:
-		fmt.Println ("Read (")
 		WalkIdent (v, ts.I)
-		fmt.Println (")")
 	case *WRITE:
-		fmt.Println ("Write(")
 		WalkExpr (v, ts.E)
-		fmt.Println (")")
 	case *REPEAT:
-		fmt.Println ("Repeat(")
 		WalkStmt(v,ts.SL)
 		WalkExpr(v,ts.C)
-		fmt.Println (")")
 	case *ASSIGN:
-		fmt.Println("Assign(")
 		WalkIdent(v,ts.I)
 		WalkExpr(v,ts.E)
-		fmt.Println(")")
 	}
 }
 func WalkExpr (v Visitor, e Expr) {
 	v.VisitExpr(e)          // action 
-	fmt.Println("Expr(")	
 	switch te := e.(type) { // type assertion
 	case *SIMPLEEXPR:
 		WalkSimpleExpr(v,te.S)
@@ -58,12 +47,10 @@ func WalkExpr (v Visitor, e Expr) {
 		WalkSimpleExpr(v,te.SE1)
 		WalkSimpleExpr(v,te.SE2)
 	}
-	fmt.Println(")")		
 }
 
 func WalkSimpleExpr(v Visitor, se SimpleExpr) {
 	v.VisitSimpleExpr(se)
-	fmt.Println("SimpleExpr(")
 	switch tse :=se.(type) {
 	case *TERM:
 		WalkTerm(v,tse.T)
@@ -72,47 +59,35 @@ func WalkSimpleExpr(v Visitor, se SimpleExpr) {
 		WalkSimpleExpr(v,tse.SE)
 		WalkTerm(v,tse.T)
 	}
-	fmt.Println(")")
 }
 
 
 
 func WalkAddOp(v Visitor, ao AddOp) {
-	fmt.Println("AddOp(")
 	v.VisitAddOp(ao)
-	fmt.Println(")")	
 }
 
 
 func WalkMulOp(v Visitor, mo MulOp) {
-	fmt.Println("MulOp(")
 	v.VisitMulOp(mo)
-	fmt.Println(")")	
 }
 
 
 func WalkCompareOp(v Visitor, co CompareOp) {
-	fmt.Println("CompareOp(")
 	v.VisitCompareOp (co)
-	fmt.Println(")")	
 }
 
 func WalkIdent (v Visitor, i Ident) {
-	fmt.Println("Ident(")
 	v.VisitIdent (i)
-	fmt.Println(")")	
 }
 
 
 func WalkNumber (v Visitor, n Number) {
-	fmt.Println("Number(")
 	v.VisitNumber (n)
-	fmt.Println(")")		
 }
 
 
 func WalkFactor(v Visitor, f Factor) { // fill
-	fmt.Println("Term(")           // action
 	v.VisitFactor(f)
 	switch tf := f.(type) {
 	case *ID:
@@ -122,13 +97,11 @@ func WalkFactor(v Visitor, f Factor) { // fill
 	case *NUMBER:
 		WalkNumber(v,tf.N)
 	}
-	fmt.Println(")")
 }
 
 
 
 func WalkTerm(v Visitor, t Term) {
-	fmt.Println("Term(")
 	v.VisitTerm(t)                // action
 	switch tt := t.(type) {
 	case *FACTOR:
@@ -138,14 +111,11 @@ func WalkTerm(v Visitor, t Term) {
 		WalkTerm(v,tt.T)
 		WalkFactor(v,tt.F)
 	}
-	fmt.Println(")")
 }
 
 
 func WalkSymbol(v Visitor, s Symbol) {
-	fmt.Println("Symbol(")
 	v.VisitSymbol(s)
-	fmt.Println(")")
 }
 
 
